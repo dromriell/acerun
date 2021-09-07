e96163caae77965e41e9036f5518e1ff64db790c
dromriell <devonromriell@gmail.com>
2021-09-06 23:53:50 -0400

    - Create AuthStack for user authentication - Change exports

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Platform } from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
+import { useSelector } from "react-redux";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+
+import AuthScreen from "../screens/auth/AuthScreen";
 
 import HomeScreen from "../screens/HomeScreen";
 
@@ -34,6 +35,7 @@ import AppColors from "../utils/AppColors";
 
 const Tab = createBottomTabNavigator();
 const DiscStack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator();
 
 const defaultStackNavOptions = {
   headerStyle: {
@@ -48,6 +50,18 @@ const defaultStackNavOptions = {
   },
   headerTintColor:
     Platform.OS === "android" ? AppColors.white : AppColors.primary,
+};
+
+const defaultAuthNavOptions = {
+  headerShown: false,
+};
+
+export const AuthStackNavigator = () => {
+  return (
+    <AuthStack.Navigator screenOptions={defaultAuthNavOptions}>
+      <AuthStack.Screen name="Login" component={AuthScreen} />
+    </AuthStack.Navigator>
+  );
 };
 
 const DiscStackNavigator = () => {
@@ -82,78 +96,74 @@ const DiscStackNavigator = () => {
   );
 };
 
-const AppNavigator = (props) => {
+export const AppNavigator = (props) => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarStyle: { backgroundColor: AppColors.primary },
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            size = 30;
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: { backgroundColor: AppColors.primary },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          size = 30;
 
-            switch (route.name) {
-              case "Home":
-                iconName = focused ? "md-home-sharp" : "md-home-outline";
-                return <Ionicons name={iconName} size={size} color={color} />;
-              case "Discs":
-                iconName = focused ? "bag-personal" : "bag-personal-outline";
-                return (
-                  <MaterialCommunityIcons
-                    name={iconName}
-                    size={size}
-                    color={color}
-                  />
-                );
-              case "Game":
-                iconName = focused ? "gamepad-round" : "gamepad-round-outline";
-                return (
-                  <MaterialCommunityIcons
-                    name={iconName}
-                    size={size}
-                    color={color}
-                  />
-                );
-              case "Courses":
-                iconName = focused ? "tree" : "tree-outline";
-                return (
-                  <MaterialCommunityIcons
-                    name={iconName}
-                    size={size}
-                    color={color}
-                  />
-                );
-              case "Profile":
-                iconName = focused
-                  ? "ios-person-circle"
-                  : "ios-person-circle-outline";
-                return <Ionicons name={iconName} size={size} color={color} />;
-              default:
-                return <Ionicons name={iconName} size={size} color={color} />;
-            }
-          },
-          tabBarActiveTintColor: AppColors.accent,
-          tabBarInactiveTintColor: AppColors.black,
-          tabBarStyle: {
-            height: 65,
-            alignItems: "center",
-            justifyContent: "center",
-          },
-          tabBarLabelStyle: {
-            paddingBottom: 10,
-          },
-        })}
-      >
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Courses" component={CoursesOverviewScreen} />
-        <Tab.Screen name="Game" component={GameSetupScreen} />
-        <Tab.Screen name="Discs" component={DiscStackNavigator} />
-        <Tab.Screen name="Home" component={HomeScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+          switch (route.name) {
+            case "Home":
+              iconName = focused ? "md-home-sharp" : "md-home-outline";
+              return <Ionicons name={iconName} size={size} color={color} />;
+            case "Discs":
+              iconName = focused ? "bag-personal" : "bag-personal-outline";
+              return (
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={size}
+                  color={color}
+                />
+              );
+            case "Game":
+              iconName = focused ? "gamepad-round" : "gamepad-round-outline";
+              return (
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={size}
+                  color={color}
+                />
+              );
+            case "Courses":
+              iconName = focused ? "tree" : "tree-outline";
+              return (
+                <MaterialCommunityIcons
+                  name={iconName}
+                  size={size}
+                  color={color}
+                />
+              );
+            case "Profile":
+              iconName = focused
+                ? "ios-person-circle"
+                : "ios-person-circle-outline";
+              return <Ionicons name={iconName} size={size} color={color} />;
+            default:
+              return <Ionicons name={iconName} size={size} color={color} />;
+          }
+        },
+        tabBarActiveTintColor: AppColors.accent,
+        tabBarInactiveTintColor: AppColors.black,
+        tabBarStyle: {
+          height: 65,
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        tabBarLabelStyle: {
+          paddingBottom: 10,
+        },
+      })}
+    >
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Courses" component={CoursesOverviewScreen} />
+      <Tab.Screen name="Game" component={GameSetupScreen} />
+      <Tab.Screen name="Discs" component={DiscStackNavigator} />
+      <Tab.Screen name="Home" component={HomeScreen} />
+    </Tab.Navigator>
   );
 };
-
-export default AppNavigator;
