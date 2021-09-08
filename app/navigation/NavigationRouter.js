59bdeb9f4ac54983a5721d4c4643585fef11e14a
dromriell <devonromriell@gmail.com>
2021-09-08 18:12:58 -0400

    - Remove commented out code - Remove navRef and isAuth vars

--- app/navigation/NavigationRouter.js
+++ app/navigation/NavigationRouter.js
@@ -1,23 +1,12 @@
 import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import { NavigationActions } from "react-navigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthStackNavigator, AppNavigator } from "./AppNavigator";
 
 const NavigationRouter = (props) => {
-  const navRef = useRef();
-  const isAuth = false;
-  //   const isAuth = useSelector((state) => {
-  //     return !!state.auth.token;
-  //   });
-
-  //   useEffect(() => {
-  //     if (!isAuth && navRef) {
-  //       navRef.current.dispatch(
-  //         NavigationActions.navigate({ routeName: "Auth" })
-  //       );
-  //     }
-  //   }, [isAuth]);
+  const isAuth = useSelector((state) => {
+    return !!state.auth.token;
+  });
 
   return (
     <NavigationContainer>
