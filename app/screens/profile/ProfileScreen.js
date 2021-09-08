a7733fa1a0560eadce9fe7cb4bebf2f62a27d5b2
dromriell <devonromriell@gmail.com>
2021-09-08 18:22:06 -0400

    - Minor code format changes

--- app/screens/profile/ProfileScreen.js
+++ app/screens/profile/ProfileScreen.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
-
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useDispatch } from "react-redux";
+import * as authActions from "../../store/actions/authActions";
 
 const ProfileScreen = (props) => {
-   return (
-      <View style={styles.screen}>
-         <Text>Profile Screen</Text>
-         <Button title='Go Back Home' onPress={() => props.navigation.goBack()} />
-      </View>
-   )
-}
+  const dispatch = useDispatch();
+  return (
+    <View style={styles.screen}>
+      <Text>Profile Screen</Text>
+      <Button title="Logout" onPress={() => dispatch(authActions.logout())} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     backgroundColor: '#fff',
-     alignItems: 'center',
-     justifyContent: 'center',
-   },
- });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
-export default ProfileScreen
+export default ProfileScreen;
