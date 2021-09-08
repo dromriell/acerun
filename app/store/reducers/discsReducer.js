75ec1f465c9940797d1d2c4095859a7866fa3524
dromriell <devonromriell@gmail.com>
2021-09-08 18:21:17 -0400

    - Add cases for changing discs state

--- app/store/reducers/discsReducer.js
+++ app/store/reducers/discsReducer.js
@@ -1,16 +1,52 @@
-import { SET_DISCS } from "../actions/discsActions";
+import {
+  SET_USER_DISCS,
+  SET_OVERVIEW_DISCS,
+  SEARCH_DISCS,
+  RESET_SEARCH,
+  ADD_DISC_TO_BAG,
+  REMOVE_DISC_FROM_BAG,
+} from "../actions/discsActions";
 
 const initialState = {
   userDiscs: [],
+  overviewDiscs: [],
+  searchResults: [],
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case SET_DISCS:
-      console.log(action.userDiscs);
+    case SET_USER_DISCS:
       return {
         ...state,
         userDiscs: action.userDiscs,
+      };
+    case SET_OVERVIEW_DISCS:
+      return {
+        ...state,
+        overviewDiscs: action.overviewDiscs,
+      };
+    case SEARCH_DISCS:
+      return {
+        ...state,
+        searchResults: action.results,
+      };
+    case RESET_SEARCH:
+      return {
+        ...state,
+        searchResults: [],
+      };
+    case ADD_DISC_TO_BAG:
+      return {
+        ...state,
+        userDiscs: [...state.userDiscs, action.newDiscResponse],
+      };
+    case REMOVE_DISC_FROM_BAG:
+      const filteredArray = state.userDiscs.filter(
+        (disc) => disc.id !== action.targetDiscId
+      );
+      return {
+        ...state,
+        userDiscs: filteredArray,
       };
     default:
       return state;
