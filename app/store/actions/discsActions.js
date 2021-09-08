05a4a398d1a11503ac5a0053cf8d0c8f0254b783
dromriell <devonromriell@gmail.com>
2021-09-08 18:20:54 -0400

    - Add actions for multiple use cases

--- app/store/actions/discsActions.js
+++ app/store/actions/discsActions.js
@@ -1,17 +1,159 @@
+import { useSelector } from "react-redux";
 import { userDiscs } from "../../dummyData/userDiscs";
 
-export const SET_DISCS = "SET_DISCS";
+export const SET_USER_DISCS = "SET_USER_DISCS";
+export const SET_OVERVIEW_DISCS = "SET_OVERVIEW_DISCS";
+export const SEARCH_DISCS = "SEARCH_DISCS";
+export const RESET_SEARCH = "RESET_SEARCH";
+export const ADD_DISC_TO_BAG = "ADD_DISC_TO_BAG";
+export const REMOVE_DISC_FROM_BAG = "REMOVE_DISC_FROM_BAG";
 
-export const fetchUserDiscs = () => {
-  console.log("HERE");
-  return async (dispatch, getState) => {
-    //  const userId = getState().auth.userId;
+import { userDiscsEP, searchDiscsEP } from "../../utils/apiEndPoints";
+
+export const fetchUserDiscs = (token) => {
+  return async (dispatch) => {
+    const response = await fetch(userDiscsEP, {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      let errorMsg = "";
+
+      Object.entries(errorResponse).forEach(([key, value]) => {
+        switch (key) {
+          case "non_field_errors":
+            errorMsg = errorMsg + value;
+            return;
+          default:
+            errorMsg = errorMsg + value;
+            return;
+        }
+      });
+      throw Error(errorMsg || "An error occured!");
+    }
+
+    const userDiscResponse = await response.json();
+
+    dispatch({ type: SET_USER_DISCS, userDiscs: userDiscResponse });
+  };
+};
+
+export const fetchOverviewDiscs = () => {
+  return async (dispatch) => {
     try {
-      console.log("Fetching...");
       const response = userDiscs;
-      dispatch({ type: SET_DISCS, userDiscs: response });
+      dispatch({ type: SET_OVERVIEW_DISCS, overviewDiscs: response });
     } catch (error) {
       throw error;
     }
   };
+};
+
+export const searchDiscs = (token, term) => {
+  return async (dispatch) => {
+    const response = await fetch(`${searchDiscsEP}?term=${term}`, {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      let errorMsg = "";
+
+      Object.entries(errorResponse).forEach(([key, value]) => {
+        switch (key) {
+          case "non_field_errors":
+            errorMsg = errorMsg + value;
+            return;
+          default:
+            errorMsg = errorMsg + value;
+            return;
+        }
+      });
+      throw Error(errorMsg || "An error occured!");
+    }
+
+    const resultsResponse = await response.json();
+
+    dispatch({ type: SEARCH_DISCS, results: resultsResponse });
+  };
+};
+
+export const addDiscToUserBag = (token, profileId, discId) => {
+  return async (dispatch) => {
+    const response = await fetch(userDiscsEP, {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        profile: profileId,
+        disc: discId,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      let errorMsg = "";
+
+      Object.entries(errorResponse).forEach(([key, value]) => {
+        switch (key) {
+          case "non_field_errors":
+            errorMsg = errorMsg + value;
+            return;
+          default:
+            errorMsg = errorMsg + value;
+            return;
+        }
+      });
+      throw Error(errorMsg || "An error occured!");
+    }
+
+    const newDiscResponse = await response.json();
+    console.log("disc added");
+    dispatch({ type: ADD_DISC_TO_BAG, newDisc: newDiscResponse });
+  };
+};
+
+export const removeDiscFromUserBag = (token, userDiscId) => {
+  // Note that this takes the userDisc id, not the disc id.
+  return async (dispatch) => {
+    const response = await fetch(`${userDiscsEP}${userDiscId}/`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      let errorMsg = "";
+
+      Object.entries(errorResponse).forEach(([key, value]) => {
+        switch (key) {
+          case "non_field_errors":
+            errorMsg = errorMsg + value;
+            return;
+          default:
+            errorMsg = errorMsg + value;
+            return;
+        }
+      });
+      throw Error(errorMsg || "An error occured!");
+    }
+
+    dispatch({ type: REMOVE_DISC_FROM_BAG, targetDiscId: userDiscId });
+  };
+};
+
+export const resetSearch = () => {
+  return { type: RESET_SEARCH };
 };
