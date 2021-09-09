a99d21d742e42e1ef7d8a11448f3abcb1726ffab
dromriell <devonromriell@gmail.com>
2021-09-08 23:07:40 -0400

    - Add EventWidget - Add container style - Refactor screen style

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
+import EventWidget from "../components/events/EventWidget";
 
 const HomeScreen = (props) => {
-   return (
-      <View style={styles.screen}>
-         <Text>Home Screen</Text>
+  return (
+    <View style={styles.screen}>
+      <View style={styles.container}>
+        <EventWidget />
       </View>
-   )
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-   screen: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-   }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+  },
+  container: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default HomeScreen
+export default HomeScreen;
