6a086362f82dafd35fa52c87ea4356acc2354ddc
dromriell <devonromriell@gmail.com>
2021-09-08 23:09:44 -0400

    - Add comment dividers to endpoint collections

--- app/utils/apiEndPoints.js
+++ app/utils/apiEndPoints.js
@@ -8,8 +8,14 @@ export const uri = `http://${manifest.debuggerHost // DEVELOPMENT URI
 
 export const authEP = `${uri}/api-token-auth/`;
 
+// DISC ENDPOINTS
+
 export const discDetailEP = `${uri}/api/v1/discs/list/`;
 
 export const userDiscsEP = `${uri}/api/v1/discs/user-discs/`;
 
 export const searchDiscsEP = `${uri}/api/v1/discs/search/`;
+
+// EVENT ENDPOINTS
+
+export const eventHomeEP = `${uri}/api-pdga/auth/?events=`; // Param is state
