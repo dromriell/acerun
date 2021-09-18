2830cd37f6e1b9a1605e845c35597eb77d9937bc
dromriell <devonromriell@gmail.com>
2021-09-18 00:10:04 -0400

    - Add coordinate vars from holeData - Add marker components for basket and tee box - Create style for markers

--- app/components/games/GameMap.js
+++ app/components/games/GameMap.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 import { SubHeaderText } from "../ui/AppText";
 
 const GameMap = (props) => {
   const { holeData } = props;
+  const { tee_box, center, basket } = holeData;
+
   const mapRef = useRef();
   const mapRegion = {
     latitude: holeData.center.lat,
@@ -17,12 +19,12 @@ const GameMap = (props) => {
     console.log("MAP HOLE DATA", holeData);
     mapRef.current.setMapBoundaries(
       {
-        latitude: holeData.center.lat + 0.0005,
-        longitude: holeData.center.lng - 0.0005,
+        latitude: center.lat + 0.0005,
+        longitude: center.lng - 0.0005,
       },
       {
-        latitude: holeData.center.lat - 0.0005,
-        longitude: holeData.center.lng + 0.0005,
+        latitude: center.lat - 0.0005,
+        longitude: center.lng + 0.0005,
       }
     );
   }, []);
@@ -36,13 +38,35 @@ const GameMap = (props) => {
       maxZoomLevel={22} // default => 20
       mapType="satellite"
       scrollEnabled={true}
-    />
+    >
+      <Marker coordinate={{ latitude: basket.lat, longitude: basket.lng }}>
+        <Image
+          source={require("../../assets/icons/map/disc-golf-basket.png")}
+          resizeMethod="resize"
+          resizeMode="contain"
+          style={{ height: 50, width: 50 }}
+        />
+      </Marker>
+      <Marker coordinate={{ latitude: tee_box.lat, longitude: tee_box.lng }}>
+        <Image
+          source={require("../../assets/icons/map/thrust-bend.png")}
+          resizeMethod="resize"
+          resizeMode="contain"
+          style={{ height: 50, width: 50 }}
+        />
+      </Marker>
+    </MapView>
   );
 };
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+  },
+  markerIcon: {
+    height: 15,
+    width: 15,
+    resizeMode: "contain",
   },
 });
 
