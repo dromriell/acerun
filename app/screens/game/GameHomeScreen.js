30e4688c26af959fc71e086113beb8510790e338
dromriell <devonromriell@gmail.com>
2021-09-18 00:18:09 -0400

    - Add useEffect to dispatch check on current game status - Refactor conditional return to game btn to use isGameInProgress state

--- app/screens/game/GameHomeScreen.js
+++ app/screens/game/GameHomeScreen.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Button } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import * as gameActions from "../../store/actions/gameActions";
 
 import { HeaderText } from "../../components/ui/AppText";
 
 const GameHomeScreen = (props) => {
-  const gameID = useSelector((state) => state.game.gameID);
+  const dispatch = useDispatch();
+  const [isGameInProgress, setIsGameInProgress] = useState(false);
+  const token = useSelector((state) => state.auth.token);
+  const userID = useSelector((state) => state.auth.profile.user);
+
+  useEffect(() => {
+    const checkIfGameInProgress = async () => {
+      const currentGame = await AsyncStorage.getItem("currentGame");
+      if (!currentGame) {
+        return;
+      }
+      const transformedData = JSON.parse(currentGame);
+      console.log(transformedData);
+      const { course, id } = transformedData.gameData;
+
+      dispatch(gameActions.fetchCurrentGameData(token, id, userID));
+      dispatch(gameActions.setCourseGameData(token, course));
+
+      setIsGameInProgress(true);
+    };
+    checkIfGameInProgress();
+  }, [dispatch]);
 
   return (
     <View style={styles.screen}>
@@ -14,10 +38,12 @@ const GameHomeScreen = (props) => {
         title="Start New Game"
         onPress={() => props.navigation.navigate("GameSetup")}
       />
-      {gameID ? (
+      {isGameInProgress ? (
         <Button
           title="Return to Game"
-          onPress={() => props.navigation.navigate("GamePlayScreen")}
+          onPress={() =>
+            props.navigation.navigate("AppGame", { screen: "GamePlayScreen" })
+          }
         />
       ) : null}
     </View>
