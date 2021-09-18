d6b46ff685dc3f4a0043e716e5c4aa445ca3e07c
dromriell <devonromriell@gmail.com>
2021-09-18 00:14:20 -0400

    - Add UI components - Remove header options

--- app/screens/game/GameScreen.js
+++ app/screens/game/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect, Button } from "react";
 import { View, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderText } from "../../components/ui/AppText";
@@ -6,30 +6,45 @@ import { HeaderText } from "../../components/ui/AppText";
 import { Ionicons } from "@expo/vector-icons";
 
 import GameMap from "../../components/games/GameMap";
+import GameHeader from "../../components/games/ui/GameHeader";
+import GameActionMenu from "../../components/games/ui/GameActionMenu";
+import StrokeMenu from "../../components/games/ui/StrokeMenu";
+
 import AppColors from "../../utils/AppColors";
 
 const GameScreen = (props) => {
+  const [isStrokeMenuOpen, setIsStrokeMenuOpen] = useState(false);
+
   const courseData = useSelector((state) => state.game.courseData);
   const [currentHoleIndex, setCurrentHoleIndex] = useState(0);
+
+  const holeData = courseData.holes[currentHoleIndex];
 
   useEffect(() => {
     console.log(courseData);
   }, []);
 
+  const onStrokeMenuPress = () => {
+    setIsStrokeMenuOpen(!isStrokeMenuOpen);
+  };
+
   return (
     <View style={styles.mapContainer}>
-      <GameMap holeData={courseData.holes[currentHoleIndex]} />
+      <GameHeader
+        navigation={props.navigation}
+        courseName={courseData.name}
+        holeData={holeData}
+      />
+      <GameActionMenu onStrokeMenuPress={onStrokeMenuPress} />
+      <StrokeMenu isStrokeMenuOpen={isStrokeMenuOpen} />
+      <GameMap holeData={holeData} />
     </View>
   );
 };
 
 export const screenOptions = (navData) => {
   return {
-    headerTitle: "",
-    headerLeft: () => null,
-    headerRight: () => {
-      <Ionicons name="menu" size={24} color={AppColors.accent} />;
-    },
+    headerShown: false,
   };
 };
 
