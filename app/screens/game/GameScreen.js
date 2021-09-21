9219bdca7a59b8947e082750ce836d8a2d207207
dromriell <devonromriell@gmail.com>
2021-09-21 15:15:11 -0400

    - Add GameScoreModal for game end display - Add conditional return if missing data

--- app/screens/game/GameScreen.js
+++ app/screens/game/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useLayoutEffect, Button } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderText } from "../../components/ui/AppText";
 
@@ -9,13 +9,24 @@ import GameMap from "../../components/games/GameMap";
 import GameHeader from "../../components/games/ui/GameHeader";
 import GameActionMenu from "../../components/games/ui/GameActionMenu";
 import HoleEndModal from "../../components/games/ui/HoleEndModal";
+import GameScorecardModal from "../../components/games/GameScorecardModal";
 
 import AppColors from "../../utils/AppColors";
 
 const GameScreen = (props) => {
   const [isHoleEndModalOpen, setIsHoleEndModalOpen] = useState(false);
+  const [isGameEnd, setIsGameEnd] = useState(false);
   const courseData = useSelector((state) => state.game.courseData);
   const currentHoleIndex = useSelector((state) => state.game.currentHoleIndex);
+  const userScorecard = useSelector((state) => state.game.scorecard);
+
+  if (!courseData) {
+    return (
+      <View style={styles.mapContainer}>
+        <ActivityIndicator size="large" color={AppColors.accent} />
+      </View>
+    );
+  }
 
   const holeData = courseData.holes[currentHoleIndex];
 
@@ -25,7 +36,18 @@ const GameScreen = (props) => {
         <HoleEndModal
           holeData={holeData}
           setIsHoleEndModalOpen={setIsHoleEndModalOpen}
+          setIsGameEnd={setIsGameEnd}
         />
+      )}
+      {isGameEnd && (
+        <View style={styles.centeredView}>
+          <GameScorecardModal
+            isGameEnd={true}
+            setIsGameEnd={setIsGameEnd}
+            navigation={props.navigation}
+            userScorecard={userScorecard}
+          />
+        </View>
       )}
       <GameHeader
         navigation={props.navigation}
@@ -52,6 +74,14 @@ const styles = StyleSheet.create({
   mapContainer: {
     width: "100%",
     height: "100%",
+  },
+  centeredView: {
+    flex: 1,
+    position: "absolute",
+    top: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
   },
 });
 
