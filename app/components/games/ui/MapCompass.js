3e90e7591315b4091647c4749c11d5c661238f54
dromriell <devonromriell@gmail.com>
2021-09-22 21:58:45 -0400

    - First commit - Create a comp that takes a heading in deg and displays a cardinal dir

---
+++ app/components/games/ui/MapCompass.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import { FontAwesome } from "@expo/vector-icons";
+
+import { BodyText } from "../../ui/AppText";
+import AppColors from "../../../utils/AppColors";
+
+const MapCompass = (props) => {
+  const degree = props.degree - 45;
+  return (
+    <View style={styles.compass}>
+      <View style={{ ...styles.direction, ...styles.north }}>
+        <BodyText size={10} color={AppColors.white}>
+          N
+        </BodyText>
+      </View>
+      <View style={{ ...styles.direction, ...styles.south }}>
+        <BodyText size={10} color={AppColors.white}>
+          S
+        </BodyText>
+      </View>
+      <View style={{ ...styles.direction, ...styles.east }}>
+        <BodyText size={10} color={AppColors.white}>
+          E
+        </BodyText>
+      </View>
+      <View style={{ ...styles.direction, ...styles.west }}>
+        <BodyText size={10} color={AppColors.white}>
+          W
+        </BodyText>
+      </View>
+      <View
+        style={{
+          ...styles.icon,
+          transform: [{ rotateZ: `${degree}deg` }],
+        }}
+      >
+        <FontAwesome name="location-arrow" size={15} color={AppColors.accent} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  compass: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 45,
+    aspectRatio: 1,
+    backgroundColor: AppColors.darkGrey,
+    borderRadius: 5,
+    padding: 2,
+  },
+  direction: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  north: {
+    alignItems: "center",
+  },
+  south: {
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  east: {
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  west: {
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  icon: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    transform: [{ rotateZ: "-45deg" }],
+  },
+});
+
+export default MapCompass;
