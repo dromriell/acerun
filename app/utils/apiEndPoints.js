e2df12c71a0978c4dc15faa96ed0e4e1433c8604
dromriell <devonromriell@gmail.com>
2021-09-28 14:24:59 -0400

    - Add searchCoursesEP - Move nearbyCoursesEP to courses subsection

--- app/utils/apiEndPoints.js
+++ app/utils/apiEndPoints.js
@@ -27,17 +27,20 @@ export const eventHomeEP = `${uri}/api-pdga/auth/?events=`; // Param is state
 // GAME ENDPOINTS
 
 export const gameHistoryEP = `${uri}/api/v1/games/game-summary/`;
-export const nearbyCoursesEP = (location) => {
-  return `${uri}/api-pdga/auth/?courses&lat=${location.lat}&lng=${location.lng}`;
-};
 export const createGameEP = `${uri}/api/v1/games/list/`;
 export const gameCourseDataEP = (courseId) =>
   `${uri}/api/v1/courses/list/${courseId}`;
 export const currentGameEP = (gameId) => `${uri}/api/v1/games/list/${gameId}/`;
 export const holeScoreEP = `${uri}/api/v1/games/hole_scores/`;
 
-// THIRD PARTY ENDPOINTS
+// COURSES ENDPOINTS
+export const nearbyCoursesEP = (location) => {
+  return `${uri}/api-pdga/auth/?courses&lat=${location.lat}&lng=${location.lng}`;
+};
+export const searchCoursesEP = (term) =>
+  `${uri}/api-pdga/auth/?courses-search&search-term=${term}`;
 
+// ----- THIRD PARTY ENDPOINTS -----
 // Weather
 export const weatherEP = (zipCode) =>
   `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${REACT_APP_OPEN_WEATHER_API_KEY}`;
