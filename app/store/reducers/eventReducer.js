731c9dc14bd0f975213e10174201469f6bc994f0
dromriell <devonromriell@gmail.com>
2021-10-19 15:01:33 -0400

    - Add handling for PDGA access denied

--- app/store/reducers/eventReducer.js
+++ app/store/reducers/eventReducer.js
@@ -7,6 +7,9 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_EVENTS:
+      if (action.events[0] === "Access denied for user anonymous") {
+        return { ...state };
+      }
       return {
         ...state,
         homeEvents: action.events,
