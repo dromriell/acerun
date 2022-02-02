bd332ad2dda8832bf44c184f3fd906db3d3010fe
dromriell <devonromriell@gmail.com>
2022-02-01 23:49:09 -0500

    - Add theme to app

--- app/navigation/NavigationRouter.js
+++ app/navigation/NavigationRouter.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthStackNavigator, AppNavigator } from "./AppNavigator";
+import AppTheme from "../utils/AppTheme";
 
 const NavigationRouter = (props) => {
   const isAuth = useSelector((state) => {
@@ -9,7 +10,7 @@ const NavigationRouter = (props) => {
   });
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={AppTheme}>
       {isAuth ? <AppNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
