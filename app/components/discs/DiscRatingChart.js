961c108b484d4cca3a19e6c3e7f553bed24c26b8
dromriell <devonromriell@gmail.com>
2022-02-03 20:19:53 -0500

    - Refactor var and function names - Add comments

--- app/components/discs/DiscRatingChart.js
+++ app/components/discs/DiscRatingChart.js
@@ -10,7 +10,7 @@ import {
 } from "victory-native";
 import AppColors from "../../utils/AppColors";
 
-const flightValueRanges = {
+const ratingConstants = {
   max: {
     turn: 0,
     glide: 7,
@@ -31,23 +31,31 @@ const flightValueRanges = {
   },
 };
 
-const processData = (data) => {
-  const makeDataArray = (d) => {
-    return Object.keys(d).map((key) => {
-      const valueCount =
-        flightValueRanges.max[key] + 1 - flightValueRanges.min[key];
-      const value =
-        key !== "turn" ? d[key] / valueCount : 1 - (d[key] + 6) / valueCount;
-      return { x: key, y: value + 0.25 };
+/**
+ * Normalizes disc flight ratings to be used and displayed
+ * on a DiscRatingChart (radar chart).
+ */
+const normalizeFlightRatings = (flightData) => {
+  const normalizedFlightData = flightData.map((discRating) => {
+    return Object.keys(discRating).map((key) => {
+      const ratingRange =
+        key !== "turn"
+          ? ratingConstants.max[key] + 1 - ratingConstants.min[key]
+          : ratingConstants.max[key] - 1 + ratingConstants.min[key];
+      // Add 0.25 to value as padding for display on chart. Prevents values
+      // from overlapping at the axis centers.
+      const value = discRating[key] / ratingRange + 0.25;
+      return { x: key, y: value };
     });
-  };
-  return data.map((datum) => makeDataArray(datum));
+  });
+  return normalizedFlightData;
 };
 
 const DiscRatingChart = (props) => {
+  const { data } = props;
   const discsData =
-    props.data.length > 0
-      ? props.data.map((disc) => {
+    data.length > 0
+      ? data.map((disc) => {
           return {
             turn: disc.turn,
             glide: disc.glide,
@@ -57,8 +65,8 @@ const DiscRatingChart = (props) => {
         })
       : [{ turn: 0, glide: 0, fade: 0, speed: 0 }];
 
-  const data = processData(discsData);
-  const maxima = flightValueRanges.max;
+  const discChartData = normalizeFlightRatings(discsData);
+  const maxima = ratingConstants.max;
 
   return (
     <View style={styles.container}>
@@ -69,21 +77,21 @@ const DiscRatingChart = (props) => {
       >
         <VictoryGroup
           colorScale={
-            props.data.length > 0
+            data.length > 0
               ? [AppColors.accent, "orange", "tomato"]
               : [AppColors.grey]
           }
           style={{ data: { fillOpacity: 0.2, strokeWidth: 2 } }}
         >
-          {data.map((data, i) => {
-            return <VictoryArea key={i} data={data} />;
+          {discChartData.map((disc, i) => {
+            return <VictoryArea key={i} data={disc} />;
           })}
         </VictoryGroup>
         {Object.keys(maxima).map((key, i) => {
           return (
             <VictoryPolarAxis
-              maxDomain={flightValueRanges.max[key]}
-              minDomain={flightValueRanges.min[key]}
+              maxDomain={ratingConstants.max[key]}
+              minDomain={ratingConstants.min[key]}
               key={i}
               dependentAxis
               style={{
@@ -109,7 +117,7 @@ const DiscRatingChart = (props) => {
               axisValue={i + 1}
               label={key.toUpperCase()}
               tickFormat={(t, i) => {
-                return flightValueRanges.ticks[key][i];
+                return ratingConstants.ticks[key][i];
               }}
               tickValues={[0.25, 0.75, 1.25]}
             />
